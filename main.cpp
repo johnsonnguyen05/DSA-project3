add29b3363335517cc890abdcd41df8aede15e56
johnsonnguyen05 <johnsonnguyen05@gmail.com>
2024-12-03 18:00:29 -0500

    more cleaning

--- main.cpp
+++ main.cpp
@@ -9,8 +9,49 @@ vector<string> split(const string &str, char delimiter) {
     }
     return tokens;
 }
-#include <queue> // For priority queue
 
+Node parseTSVRow(const string &row) {
+    stringstream ss(row);
+    string id, type, title, year, genresStr, altTitleStr, runtimeStr, ratingStr, numOfRatingsStr;
+
+    getline(ss, id, '\t');          // ID
+    getline(ss, type, '\t');        // Type (ignored)
+    getline(ss, title, '\t');       // Title
+    getline(ss, altTitleStr, '\t');  // Alt Title
+    getline(ss, year, '\t');        // Year
+    getline(ss, runtimeStr, '\t');  // Runtime
+    getline(ss, genresStr, '\t');   // Genres
+    getline(ss, ratingStr, '\t');   // Rating
+    getline(ss, numOfRatingsStr);   // Number of Ratings
+
+    string altTitle = altTitleStr;
+    string runtime = runtimeStr;
+    float rating = stof(ratingStr);
+    int numOfRatings = stoi(numOfRatingsStr);
+    vector<string> genres = split(genresStr, ',');
+
+    return Node(id, title, altTitle, year, runtime, genres, rating, numOfRatings);
+}
+float calculateWeight(const Node &node1, const Node &node2) {
+    // Combine ratings and number of ratings for weight calculation
+    float combinedMetric1 = node1.numOfRatings * node1.rating;
+    float combinedMetric2 = node2.numOfRatings * node2.rating;
+
+    // Avoid log(0) by ensuring the metric is > 0
+    if (combinedMetric1 <= 0 || combinedMetric2 <= 0) {
+        return 10000; // Assign max weight for unratable movies
+    }
+
+    // Compute the weight
+    float logMetric1 = log(combinedMetric1);
+    float logMetric2 = log(combinedMetric2);
+
+    float weight1 = 10000 / (logMetric1 * 3);
+    float weight2 = 10000 / (logMetric2 * 3);
+
+    // Return the average weight of the two movies
+    return (weight1 + weight2) / 2.0f;
+}
 // Function to run Dijkstra's algorithm
 vector<pair<string, float>> dijkstra(const Graph &graph, const string &startNode, int topN = 5) {
     unordered_map<string, float> distances; // Distance to each node
@@ -63,49 +104,6 @@ vector<pair<string, float>> dijkstra(const Graph &graph, const string &startNode
     }
 
     return results;
-}
-
-Node parseTSVRow(const string &row) {
-    stringstream ss(row);
-    string id, type, title, year, genresStr, altTitleStr, runtimeStr, ratingStr, numOfRatingsStr;
-
-    getline(ss, id, '\t');          // ID
-    getline(ss, type, '\t');        // Type (ignored)
-    getline(ss, title, '\t');       // Title
-    getline(ss, altTitleStr, '\t');  // Alt Title
-    getline(ss, year, '\t');        // Year
-    getline(ss, runtimeStr, '\t');  // Runtime
-    getline(ss, genresStr, '\t');   // Genres
-    getline(ss, ratingStr, '\t');   // Rating
-    getline(ss, numOfRatingsStr);   // Number of Ratings
-
-    string altTitle = altTitleStr;
-    string runtime = runtimeStr;
-    float rating = stof(ratingStr);
-    int numOfRatings = stoi(numOfRatingsStr);
-    vector<string> genres = split(genresStr, ',');
-
-    return Node(id, title, altTitle, year, runtime, genres, rating, numOfRatings);
-}
-float calculateWeight(const Node &node1, const Node &node2) {
-    // Combine ratings and number of ratings for weight calculation
-    float combinedMetric1 = node1.numOfRatings * node1.rating;
-    float combinedMetric2 = node2.numOfRatings * node2.rating;
-
-    // Avoid log(0) by ensuring the metric is > 0
-    if (combinedMetric1 <= 0 || combinedMetric2 <= 0) {
-        return 10000; // Assign max weight for unratable movies
-    }
-
-    // Compute the weight
-    float logMetric1 = log(combinedMetric1);
-    float logMetric2 = log(combinedMetric2);
-
-    float weight1 = 10000 / (logMetric1 * 3);
-    float weight2 = 10000 / (logMetric2 * 3);
-
-    // Return the average weight of the two movies
-    return (weight1 + weight2) / 2.0f;
 }
 
 int main() {
@@ -196,18 +194,18 @@ int main() {
     }
 
     if (dijsktra) {
-    cout << "You have chosen Dijkstra's algorithm." << endl;
+        cout << "You have chosen Dijkstra's algorithm." << endl;
 
-    vector<pair<string, float>> recommendations = dijkstra(movieGraph, movieNode.id);
+        vector<pair<string, float>> recommendations = dijkstra(movieGraph, movieNode.id);
 
-    cout << "Top recommendations based on shortest path:\n";
-    for (const auto &[movieId, distance] : recommendations) {
-        auto it = find_if(nodes.begin(), nodes.end(), [&](const Node &node) { return node.id == movieId; });
-        if (it != nodes.end()) {
-            cout << "Title: " << it->title << ", Distance: " << distance << endl;
+        cout << "Top recommendations based on shortest path:\n";
+        for (const auto &[movieId, distance] : recommendations) {
+            auto it = find_if(nodes.begin(), nodes.end(), [&](const Node &node) { return node.id == movieId; });
+            if (it != nodes.end()) {
+                cout << "Title: " << it->title << ", Distance: " << distance << endl;
+            }
         }
     }
-}
 
     
     if (!isFound) {
